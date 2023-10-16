11118fb7353ef7565ea47feef50214e9be0553bd
bluluke <bluluke@hotmail.com>
2023-10-16 17:55:04 +0100

    Update: WebSocket Connection.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import './App.css'
+import React, { useEffect } from 'react';
 import { Header } from './Components/Header'
-function App() {
 
+
+function App() {
+  useEffect(() => {
+    const ws = new WebSocket('ws://localhost:9020/websocket');
+
+    ws.onopen = () => {
+      console.log('WebSocket connected');
+    };
+
+    ws.onmessage = (event) => {
+      console.log('Received message:', event.data);
+    };
+
+    ws.onclose = () => {
+      console.log('WebSocket disconnected')
+    };
+  }, []);
 
   return (
     <div>
