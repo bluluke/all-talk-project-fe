48c9d77b9a03946244f7439879ef795a19493f72
bluluke <bluluke@hotmail.com>
2023-10-16 16:57:46 +0100

    Update app.jsx: message form + Header component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
-
+import { Header } from './Components/Header'
 function App() {
 
 
   return (
-    <>
-    </>
+    <div>
+      <Header />
+      <div id="chat-container">
+          <h2>Chat Name</h2>
+          <div id="messages-container">
+
+          </div>
+          <form id="message-form">
+              <input id="message-box" placeholder="write message here"></input>
+              <button id="submit-message">Submit</button>
+          </form>
+      </div> 
+    </div>
   )
 }
 
