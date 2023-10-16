37b6a38dc0bfa7544542d0a0a5248b93a79bcc46
bluluke <bluluke@hotmail.com>
2023-10-16 20:10:56 +0100

    Update: messages appear on page of sender and recipients.

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import './App.css'
 import React, { useEffect, useState } from 'react';
 import { Header } from './Components/Header'
 
+const username = prompt('Tell us your name!');
 
 function App() {
   const [message, setMessage] = useState('');
+  const [messageList, setMessageList] = useState([]);
+
   const ws = new WebSocket('ws://localhost:9020/websocket');
   useEffect(() => {
     ws.onopen = () => {
@@ -12,7 +15,8 @@ function App() {
     };
 
     ws.onmessage = (event) => {
-      console.log('Received message:', event.data);
+      const parsedEventData = JSON.parse(event.data);
+      setMessageList((prevMessageList) => [...prevMessageList, { type: parsedEventData.type, content: parsedEventData.content, username: parsedEventData.username }]);
     };
 
     ws.onclose = () => {
@@ -23,9 +27,10 @@ function App() {
   const handleSendMessage = (e) => {
     e.preventDefault()
     if(message.trim() !== '') {
-      ws.send(JSON.stringify({ type: 'user_message', content: message }));
+      ws.send(JSON.stringify({ type: 'user_message', content: message, username: username }));
 
       setMessage('');
+      console.log('messageList: ', messageList);
     }
   };
 
@@ -35,7 +40,9 @@ function App() {
       <div id="chat-container">
           <h2>Chat Name</h2>
           <div id="messages-container">
-
+            {messageList.map((message, index) => (
+              <p key={index}>{message.username}: {message.content}</p>
+            ))}
           </div>
           <form id="message-form">
               <input id="message-box" placeholder="write message here" value={message} onChange={(e) => setMessage(e.target.value)}></input>
