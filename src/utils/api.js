b2242e25ef96ebc945d75eb04c6249b22db5a1a0
bluluke <bluluke@hotmail.com>
2023-10-18 14:48:34 +0100

    Update getChats: filter response by date.

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,10 +4,22 @@ const allTalkApi = axios.create({
     baseURL: "https://all-talk-api.onrender.com/api"
 });
 
-export const getChats = () => {
+export const getChats = (fromDate, toDate) => {
     let endpoint = '/chats'
+    let fromDateQuery = '';
+    if(fromDate) {
+        fromDateQuery = 
+        endpoint += `?from_date=${fromDate}`;
+        if(toDate) {
+            endpoint += `&&to_date=${toDate}`
+        }
+    } else if (toDate) {
+        endpoint += `?to_date=${toDate}`
+    }
     return allTalkApi.get(endpoint).then((res) => {
         return res.data.chats
     })
 }
+
+
 
