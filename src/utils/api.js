61d409b0a2de7d0d5b6525143c7657e9dc4e2545
bluluke <bluluke@hotmail.com>
2023-10-18 14:26:27 +0100

    Add getChats function: return all chats from chat-list collection from all-talk-project database.

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,5 +4,10 @@ const allTalkApi = axios.create({
     baseURL: "https://all-talk-api.onrender.com/api"
 });
 
-
+export const getChats = () => {
+    let endpoint = '/chats'
+    return allTalkApi.get(endpoint).then((res) => {
+        return res.data.chats
+    })
+}
 
