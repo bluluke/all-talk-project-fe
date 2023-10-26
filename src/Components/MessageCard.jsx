1746c6d2c4b00843d9bd48ee58d6e7b82a9b355b
bluluke <bluluke@hotmail.com>
2023-10-26 18:56:55 +0100

    Update delete message: user can only delete own messages.

--- src/Components/MessageCard.jsx
+++ src/Components/MessageCard.jsx
@@ -1,6 +1,5 @@
 import { deleteMessage } from "../utils/api"
-export const MessageCard = ({_id, senderName, messageContent, timeOfSending, setMessageList, chatid}) => {
-    
+export const MessageCard = ({_id, senderName, messageContent, timeOfSending, setMessageList, chatid, username}) => {
     const handleDeleteMessage = () => {
         deleteMessage(chatid, _id).then((data) => {
         })
@@ -13,7 +12,10 @@ export const MessageCard = ({_id, senderName, messageContent, timeOfSending, set
        <div className="message-card">
             <p>{senderName}: {messageContent}</p>
             <p>{timeOfSending}</p>
+            {username === senderName && (
             <button onClick={handleDeleteMessage}>Delete</button>
-       </div>
+
+        )}
+         </div>
     )
 }
