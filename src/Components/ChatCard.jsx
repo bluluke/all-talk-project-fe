a4d3638a847071d374846039ad244bb8fc41cb55
bluluke <bluluke@hotmail.com>
2023-10-27 20:50:18 +0100

    Update delete chat: Remove chat from list.

--- src/Components/ChatCard.jsx
+++ src/Components/ChatCard.jsx
@@ -1,13 +1,21 @@
-export const ChatCard = ({ _id, chatName, timeOfcreation, username, chatCreator }) => {
+import { deleteChat, getChatsNames } from "../utils/api"
+
+
+
+export const ChatCard = ({ _id, chatName, timeOfcreation, username, chatCreator, setChatsNames }) => {
+
+
+    const handleDeletechat = () => {
+        deleteChat(_id).then(() => {
+            getChatsNames().then((chatNamesData) => {
+               setChatsNames(chatNamesData)
+            })
+        })
+    }
     return (
         <li>
             <section className="chat-card">
                 <h2 className="chat-card-heading">{chatName}</h2>
-
-             {chatCreator === username && ( 
-                    <button>Delete</button>
-                 )} 
-
             </section>
         </li>
     )
--- src/Components/ChatList.jsx
+++ src/Components/ChatList.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
-import { getChatsNames } from "../utils/api";
 import { ChatCard } from './ChatCard';
+import { deleteChat, getChatsNames } from "../utils/api"
+
 
 export const ChatList = ({username}) => {
 const [chatsNames, setChatsNames] = useState();
@@ -13,15 +14,27 @@ getChatsNames().then((data) => {
 
 })
 }, [])
+const handleDeletechat = (_id) => {
+    deleteChat(_id).then(() => {
+        getChatsNames().then((chatNamesData) => {
+           setChatsNames(chatNamesData)
+        })
+    })
+}
 if(isLoading) return <p>Loading... </p>;
 return (
     <section id="chat-list-container">
         <ul id="chat-list">
            {chatsNames.map(({ _id, chatName, timeOfCreation, chatCreator}) => {
 
-                return <Link to={`/chats/${_id}`} key={_id} className='chat-link'>
-                    <ChatCard _id={_id} chatName={chatName} timeOfCreation={timeOfCreation} chatCreator={chatCreator} username={username}/>
-                </Link>
+                return <div key={_id}>
+                    <Link to={`/chats/${_id}`} key={_id} className='chat-link'>
+                        <ChatCard _id={_id} chatName={chatName} timeOfCreation={timeOfCreation} chatCreator={chatCreator} username={username} setChatsNames={setChatsNames}/>
+                     </Link>
+                    {chatCreator === username && ( 
+                        <button onClick={() => handleDeletechat(_id)}>Delete</button>
+                    )} 
+                </div>
            })}
         </ul>
     </section>
