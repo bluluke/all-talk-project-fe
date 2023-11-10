6786d1e72794694877d4f72e9fa4f3469de1955b
bluluke <bluluke@hotmail.com>
2023-11-10 17:09:40 +0000

    Add sender name in MessageCard + this-user-message styling.

--- src/Components/MessageCard.jsx
+++ src/Components/MessageCard.jsx
@@ -24,7 +24,7 @@ export const MessageCard = ({_id, senderName, messageContent, timeOfSending, han
     if(editInProgress && idOfMessageToEdit === _id) return <p>Edit in progress...</p>
     return (
        <div className="message-card">
-            <p>{senderName}: {messageContent}</p>
+            <p><span className="sender-name">{senderName}:</span> {messageContent}</p>
             <p className="time-of-sending">{timeOfSending}</p>
             {user.user === senderName && (
             <div>
@@ -43,3 +43,9 @@ export const MessageCard = ({_id, senderName, messageContent, timeOfSending, han
        </div>
     )
 }
+
+
+
+
+
+
