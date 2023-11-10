6737541a0b3ac9c378c56bf8af56f247686b8946
bluluke <bluluke@hotmail.com>
2023-11-10 19:43:27 +0000

    Fix submit edited message form: only target edited message form appear.

--- src/pages/MessageList.jsx
+++ src/pages/MessageList.jsx
@@ -4,7 +4,7 @@ import { useContext } from 'react';
 import { UserContext } from '../contexts/User';
 
 
-export const MessageList = ({ messageList, setMessageList, handleDeleteMessage, handleEditMessage, setEditInProgress, editInProgress, idOfMessageToEdit, setIdOfMessageToEdit, deleteInProgress, setDeleteInProgress, idOfMessageToDelete, setIdOfMessageToDelete, messageSent}) => {
+export const MessageList = ({ messageList, setMessageList, handleDeleteMessage, handleEditMessage, setEditInProgress, editInProgress, idOfMessageToEdit, setIdOfMessageToEdit, idOfMessageBeingEdited, setIdOfMessageBeingEdited, deleteInProgress, setDeleteInProgress, idOfMessageToDelete, setIdOfMessageToDelete, messageSent}) => {
     const [editMessage, setEditMessage] = useState(false);
     const user = useContext(UserContext);
     const messageListRef = useRef(null);
@@ -23,7 +23,7 @@ export const MessageList = ({ messageList, setMessageList, handleDeleteMessage, 
                     if(user.user === senderName) classForMessageCard = 'this-user-message';
                     else classForMessageCard = 'other-user-message'
                     return <div key={_id} className={classForMessageCard}>
-                    <MessageCard _id={_id} senderName={senderName} messageContent={messageContent} timeOfSending={new Date(timeOfSending.$timestamp.t).toLocaleString()} setMessageList={setMessageList} handleDeleteMessage={handleDeleteMessage} handleEditMessage={handleEditMessage} setEditInProgress={setEditInProgress} editInProgress={editInProgress} idOfMessageToEdit={idOfMessageToEdit} setIdOfMessageToEdit={setIdOfMessageToEdit} deleteInProgress={deleteInProgress} setDeleteInProgress={setDeleteInProgress} idOfMessageToDelete={idOfMessageToDelete} setIdOfMessageToDelete={setIdOfMessageToDelete} editMessage={editMessage} setEditMessage={setEditMessage}/>
+                    <MessageCard _id={_id} senderName={senderName} messageContent={messageContent} timeOfSending={new Date(timeOfSending.$timestamp.t).toLocaleString()} setMessageList={setMessageList} handleDeleteMessage={handleDeleteMessage} handleEditMessage={handleEditMessage} setEditInProgress={setEditInProgress} editInProgress={editInProgress} idOfMessageToEdit={idOfMessageToEdit} setIdOfMessageToEdit={setIdOfMessageToEdit} idOfMessageBeingEdited={idOfMessageBeingEdited} setIdOfMessageBeingEdited={setIdOfMessageBeingEdited} deleteInProgress={deleteInProgress} setDeleteInProgress={setDeleteInProgress} idOfMessageToDelete={idOfMessageToDelete} setIdOfMessageToDelete={setIdOfMessageToDelete} editMessage={editMessage} setEditMessage={setEditMessage}/>
                 </div>
                 })}
             </ul>
